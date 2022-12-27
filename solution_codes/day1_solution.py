462a6626fcc0dc323e14c8a0a2a4b0f8c04c4f09
Ali Abdullah Bhatti <53481567+AliAbdullah1999@users.noreply.github.com>
2022-12-27 12:17:22 +0500

    My solutions of challenges given in 100 days of code at replit

--- solution_codes/day1_solution.py
+++ solution_codes/day1_solution.py
@@ -1,6 +1,10 @@
+#printing name
 print('Ali Abdullah')
+#printing date
 print('30/11/2022')
+#message while signing up
 print('''I am signing up for Replit's 100 days of Python challenge!
 I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
 I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!''')
+#emotions at start
 print('I am feeling :(')
